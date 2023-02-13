a135737d9a3cdab5de280abdb34b828a024aa72e
GIL <gil.00000032610@unicap.br>
2023-02-13 16:12:27 -0300

    CodingEx36 done

--- src/CodingEx36/Circle.java
+++ src/CodingEx36/Circle.java
@@ -1,5 +1,18 @@
 package CodingEx36;
 
 public class Circle {
-    double radius;
+    private double radius;
+
+    public Circle(double radius) {
+        if(radius< 0){
+            this.radius = 0;
+        }else this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+        return (radius * radius * Math.PI);
+    }
 }
---
+++ src/CodingEx37/Rectangle.java
@@ -0,0 +1,4 @@
+package CodingEx37;
+
+public class Rectangle {
+}
